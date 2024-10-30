c7350b76093b5b83e96ffc772eab914e29844194
Mosolov Anton <mosolov2000@gmail.com>
2024-10-30 13:19:13 +0000

    working ad gpio lab
    work on #3

--- main.c
+++ main.c
@@ -52,8 +52,25 @@
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "boards.h"
-#define MIN_DELAY 150
+#include "nrf_gpio.h"
+
+#define MIN_DELAY 500
 #define MAX_DELAY 1000
+#define MICRO_DELAY 100
+
+#define LED0_GREEN_PIN NRF_GPIO_PIN_MAP(0, 6)
+#define LED1_RED_PIN NRF_GPIO_PIN_MAP(0, 8)
+#define LED2_GREEN_PIN NRF_GPIO_PIN_MAP(1, 9)
+#define LED3_BLUE_PIN NRF_GPIO_PIN_MAP(0, 12)
+
+#define BUTTON_PIN NRF_GPIO_PIN_MAP(1, 6)
+
+void gpio_unit(void);
+void led_blink(int);
+
+static int led_digits[] = {LED0_GREEN_PIN, LED1_RED_PIN, LED2_GREEN_PIN, LED3_BLUE_PIN};
+static int id_digits[] = {6, 6, 1, 5};
+
 /**
  * @brief Function for application main entry.
  */
@@ -61,27 +78,76 @@ int main(void)
 {
     /* Configure board. */
     bsp_board_init(BSP_INIT_LEDS);
-    int id_digits[] = {6,6,1,5};
-    // Device ID 6615
+    gpio_unit();
+    int i_leds = 0;
+    int i_blink = 0;
+    bool is_led_on = false;
 
     /* Toggle LEDs. */
     while (true)
     {
-        for (int i = 0; i < LEDS_NUMBER; i++) 
+
+        if (nrf_gpio_pin_read(BUTTON_PIN) == 0)
         {
-            for ( int blink = 0; blink < id_digits[i]; blink++) 
+            while (true)
             {
-                bsp_board_led_invert(i);
-                nrf_delay_ms(MIN_DELAY);
-                bsp_board_led_invert(i);
-                nrf_delay_ms(MIN_DELAY);
+
+                if (i_leds >= LEDS_NUMBER)
+                    i_leds = 0;
+
+                while (i_blink < id_digits[i_leds])
+                {
+                    if (is_led_on == true)
+                        nrf_gpio_pin_write(led_digits[i_leds], 1);
+
+                    nrf_gpio_pin_write(led_digits[i_leds], 0);
+                    is_led_on = true;
+
+                    if (nrf_gpio_pin_read(BUTTON_PIN))
+                        break;
+
+                    nrf_delay_ms(MIN_DELAY);
+
+                    nrf_gpio_pin_write(led_digits[i_leds], 1);
+                    is_led_on = false;
+
+                    if (nrf_gpio_pin_read(BUTTON_PIN))
+                        break;
+
+                    nrf_delay_ms(MIN_DELAY);
+
+                    i_blink++;
+                }
+
+                if (nrf_gpio_pin_read(BUTTON_PIN))
+                    break;
+
+                i_blink = 0;
+                i_leds++;
             }
-            nrf_delay_ms(MAX_DELAY);
-           
         }
     }
 }
+void gpio_unit()
+{
+    nrf_gpio_cfg_input(BUTTON_PIN, NRF_GPIO_PIN_PULLUP); // конфигурация кнопки в качестве входа с подтягивающим вверх резистором
 
-/**
- *@}
- **/
+    nrf_gpio_cfg_output(LED0_GREEN_PIN); // LED0
+    nrf_gpio_cfg_output(LED1_RED_PIN);   // LED1
+    nrf_gpio_cfg_output(LED2_GREEN_PIN); // LED2
+    nrf_gpio_cfg_output(LED3_BLUE_PIN);  // LED3
+    for (int i = 0; i < LEDS_NUMBER; i++)
+    {
+        nrf_gpio_pin_write(led_digits[i], 1);
+    }
+}
+/*
+     void led_blink(int i)
+     {
+         nrf_gpio_pin_write(led_digits[i], 0);
+         nrf_delay_ms(MIN_DELAY);
+         nrf_gpio_pin_write(led_digits[i], 1);
+         nrf_delay_ms(MIN_DELAY);
+     }
+
+ */
