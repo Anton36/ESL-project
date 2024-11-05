d91e302fc4627e85ade0672eb2e2e83ec76d77c2
Mosolov Anton <mosolov2000@gmail.com>
2024-11-05 20:05:28 +0000

    4.1 lab ready

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "boards.h"
 #include "nrf_gpio.h"
 
+#include "nrfx_systick.h"
+
 #define MIN_DELAY 500
 #define MAX_DELAY 1000
 #define MICRO_DELAY 100
+
+#define PWM_PERIOD_US 1000
+#define PWM_STEP 1
 
 #define LED_ON_STATE 0
 #define LED_OFF_STATE 1
@@ -24,7 +28,8 @@
 void gpio_unit(void);
 void led_on(int);
 void led_off(int);
-void blinking(int);
+
+void systick_pwm(int);
 
 static int led_digits[] = {LED0_GREEN_PIN, LED1_RED_PIN, LED2_GREEN_PIN, LED3_BLUE_PIN};
 static int id_digits[] = {6, 6, 1, 5};
@@ -39,14 +44,15 @@ int main(void)
     /* Configure board. */
     bsp_board_init(BSP_INIT_LEDS);
     gpio_unit();
+    nrfx_systick_init();
 
     /* Toggle LEDs. */
     while (true)
     {
         if (nrf_gpio_pin_read(BUTTON_PIN) == BUTTON_PRESSED_STATE)
         {
-            
-            blinking(i_leds);
+            systick_pwm(i_leds);
+            nrf_delay_ms(MICRO_DELAY);
             i_blink++;
             if (i_blink >= id_digits[i_leds])
             {
@@ -84,19 +90,39 @@ void led_off(int led_index)
     nrf_gpio_pin_write(led_digits[led_index], LED_OFF_STATE);
 }
 
-void blinking(int led_index)
-{
-    led_on(led_index);
 
-    for (size_t i = 0; i < MIN_DELAY / 100; i++)
-    {
-        if (nrf_gpio_pin_read(BUTTON_PIN) == BUTTON_UNPRESSED_STATE)
+
+void systick_pwm(int led_index)
+{
+    int duty_cycle = 0;
+    nrfx_systick_state_t pwm_time;
+    
+    
+
+        for (duty_cycle = 0; duty_cycle <= PWM_PERIOD_US; duty_cycle += PWM_STEP)
         {
-            return;
+            nrfx_systick_get(&pwm_time);
+            led_on(led_index);
+            while (!nrfx_systick_test(&pwm_time, duty_cycle))
+            {
+            }
+            led_off(led_index);
+            while (!nrfx_systick_test(&pwm_time, PWM_PERIOD_US - duty_cycle))
+            {
+            }
         }
-        nrf_delay_ms(MICRO_DELAY);
-    }
-    led_off(led_index);
-    nrf_delay_ms(MIN_DELAY);
+
+        for (duty_cycle = PWM_PERIOD_US; duty_cycle > 0; duty_cycle -= PWM_STEP)
+        {
+            nrfx_systick_get(&pwm_time);
+            led_on(led_index);
+            while (!nrfx_systick_test(&pwm_time, duty_cycle))
+            {
+            }
+            led_off(led_index);
+            while (!nrfx_systick_test(&pwm_time, PWM_PERIOD_US - duty_cycle))
+            {
+            }
+        }
     
 }
