9c15da38d8ae5d81834cb6ecc3f068ca1a1f0f63
Mosolov Anton <mosolov2000@gmail.com>
2024-11-11 18:39:21 +0000

    Lab 4 Finally ready ?loging in progress

--- main.c
+++ main.c
@@ -11,6 +11,12 @@
 
 #include "nrfx_systick.h"
 
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+#include "nrf_log_backend_usb.h"
+
 #define MIN_DELAY 500
 #define MAX_DELAY 1000
 #define MICRO_DELAY 100
@@ -40,11 +46,14 @@ void timer_init(void);
 void lfclk_request(void);
 void systick_pwm(int);
 void clock_event_handler(nrfx_clock_evt_type_t event);
+void maintain_pwm(int led_index, int duty_cycle);
+void logs_init();
 
 volatile bool double_click = false;
 volatile int number_of_click = 0;
 volatile bool debounce_timer_active = false;
 volatile bool doubleclick_timer_active = false;
+volatile int current_duty_cycle = 0;
 
 APP_TIMER_DEF(debounce_timer);
 APP_TIMER_DEF(double_click_timer);
@@ -67,20 +76,29 @@ int main(void)
     nrfx_systick_init();
     nrfx_gpiote_init();
     gpio_init();
+    logs_init();
+    NRF_LOG_INFO("Starting up the test project with USB logging");
 
     /* Toggle LEDs. */
     while (true)
     {
-        while (double_click)
+        if (double_click)
         {
             systick_pwm(i_leds);
 
             i_blink++;
+            NRF_LOG_INFO("i_blink++");
+            LOG_BACKEND_USB_PROCESS();
+            NRF_LOG_PROCESS();
             if (i_blink >= id_digits[i_leds])
             {
                 i_blink = 0;
                 i_leds = (i_leds + 1) % LEDS_NUMBER;
             }
+        }
+        else if ((double_click == 0) & (current_duty_cycle != 0) )
+        {
+            maintain_pwm(i_leds, current_duty_cycle);
         }
     }
 }
@@ -155,26 +173,37 @@ void led_off(int led_index)
 
 void systick_pwm(int led_index)
 {
-    int duty_cycle = 0;
+    int duty_cycle = current_duty_cycle;
     nrfx_systick_state_t pwm_time;
 
-    for (duty_cycle = 0; duty_cycle <= PWM_PERIOD_US; duty_cycle += PWM_STEP)
+    for (; duty_cycle <= PWM_PERIOD_US; duty_cycle += PWM_STEP)
     {
+        current_duty_cycle = duty_cycle;
         led_off(led_index);
         nrfx_systick_get(&pwm_time);
         led_on(led_index);
         while (!nrfx_systick_test(&pwm_time, duty_cycle))
         {
+             if (double_click == 0)
+            {
+                return;
+            }
         }
         nrfx_systick_get(&pwm_time);
         led_off(led_index);
         while (!nrfx_systick_test(&pwm_time, PWM_PERIOD_US - duty_cycle))
         {
+             if (double_click == 0)
+            {
+                return;
+            }
         }
     }
+    duty_cycle = current_duty_cycle;
 
-    for (duty_cycle = PWM_PERIOD_US; duty_cycle > 0; duty_cycle -= PWM_STEP)
+    for (; duty_cycle >= 0; duty_cycle -= PWM_STEP)
     {
+        current_duty_cycle = duty_cycle;
         led_off(led_index);
         nrfx_systick_get(&pwm_time);
         led_on(led_index);
@@ -194,6 +223,31 @@ void systick_pwm(int led_index)
                 return;
             }
         }
+    }
+}
+
+void maintain_pwm(int led_index, int duty_cycle)
+{
+    nrfx_systick_state_t pwm_time;
+
+    led_off(led_index);
+    nrfx_systick_get(&pwm_time);
+    led_on(led_index);
+    while (!nrfx_systick_test(&pwm_time, duty_cycle))
+    {
+       if (double_click == 1)
+            {
+                return;
+            }
+    }
+    nrfx_systick_get(&pwm_time);
+    led_off(led_index);
+    while (!nrfx_systick_test(&pwm_time, PWM_PERIOD_US - duty_cycle))
+    {
+        if (double_click == 1)
+            {
+                return;
+            }
     }
 }
 void timer_init(void)
@@ -217,4 +271,12 @@ void lfclk_request(void)
     nrfx_clock_init(clock_event_handler);
     nrfx_clock_enable();
     nrfx_clock_lfclk_start();
+}
+
+void logs_init()
+{
+    NRF_LOG_INIT(NULL);
+    
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
 }
